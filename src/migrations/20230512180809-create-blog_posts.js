086c40e0eb41a0ef5e8fff0ee8230b968bb5feca
André Resende <andreresende36@gmail.com>
2023-05-12 15:50:44 -0300

    feature 1: done

---
+++ src/migrations/20230512180809-create-blog_posts.js
@@ -0,0 +1,44 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('blog_posts', {
+      id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      published: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      updated: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('blog_posts');
+  }
+};
---
+++ src/migrations/20230512180819-create-posts_categories.js
@@ -0,0 +1,40 @@
+'use strict';
+
+  /**
+   *
+   * @param {import('sequelize').QueryInterface} queryInterface
+   * @param {import('sequelize')} Sequelize
+   */
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('posts_categories', { 
+      post_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'blog_posts',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+      category_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'categories',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('posts_categories');
+  }
+};
